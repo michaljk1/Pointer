0fdfb89b9407405f4ad5a7c7b590c0c50cb803f0
michaljk1 <michal.januszek98@gmail.com>
2020-07-18 12:10:13 +0200

    almost finish v6

---
+++ README.md
@@ -0,0 +1,5 @@
+python3 -m venv venv
+virtualenv venv
+venv/bin/activate
+pip install flask
+pip install -r requirements.txt
--- pointer.py
+++ pointer.py
@@ -2,14 +2,3 @@ from app import create_app
 
 app = create_app()
 
-
-from app.models.usercourse import User
-from app import db
-with app.app_context():
-    mod = User.query.filter_by(role='MODERATOR').all()
-    if len(mod) == 0:
-        user = User(email='mod@admin.com', login='moderator', name='Moderator', surname='Moderator',
-                    role='MODERATOR', is_confirmed=True)
-        user.set_password('Admin123')
-        db.session.add(user)
-        db.session.commit()
