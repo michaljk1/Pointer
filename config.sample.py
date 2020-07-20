d15ae7ea07da13718dd3c5b449517447f33890b1
michaljk1 <47529163+michaljk1@users.noreply.github.com>
2020-07-20 19:59:26 +0200

    Update config.sample.py

--- config.sample.py
+++ config.sample.py
@@ -4,23 +4,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = 'sample'
-    SQLALCHEMY_DATABASE_URI = 'sample'
+    SECRET_KEY = 'change-it-not-to-be-guessed'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #local db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_URL = 'sample'
+    REDIS_URL = 'redis://' #local server
     # Flask-Mail SMTP server settings
     MAIL_SERVER = 'sample'
     MAIL_PORT = 'sample'
     MAIL_USE_TLS = 'sample'
     SERVER_NAME = '127.0.0.1:5000'
 
-    INSTANCE_DIR = '/home/michal/PycharmProjects/Pointer/instance'
-    MAX_MEMORY_MB = 3123#sample
+    INSTANCE_DIR = '/home/mich/Pointer/instance'
+    MAX_MEMORY_MB = 5000#sample
 
     #POINTER EMAIL SENDER
-    MAIL_USERNAME = 'sample'
-    MAIL_PASSWORD = 'sample'
-    ADMINS = ['sample']
+    MAIL_USERNAME = 'pointerlicmail@gmail.com'
+    MAIL_PASSWORD = ''
+    ADMINS = ['pointerlicmail@gmail.com']
 
     #MODERATOR CREDENTIALS
     MOD_EMAIL = 'moderator@admin.com'
