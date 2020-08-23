6065e503cede862d24dad920abf943759882baf0
michaljk1 <theNeweSTls1>
2020-08-23 17:32:43 +0200

    append to course moved to student routes

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -62,11 +62,11 @@ def login():
             flash('Nieprawidłowe dane', 'error')
             db.session.commit()
             return redirect(url_for('auth.login'))
-        # if not user.is_confirmed:
-        #     login_info.status = LoginInfo.Status['ERROR']
-        #     flash('Aktywuj swoje konto')
-        #     db.session.commit()
-        #     return redirect(url_for('auth.activate'))
+        if not user.is_confirmed:
+            login_info.status = LoginInfo.Status['ERROR']
+            flash('Aktywuj swoje konto')
+            db.session.commit()
+            return redirect(url_for('auth.activate'))
         login_user(user, remember=form.remember_me.data)
         db.session.commit()
         next_page = request.args.get('next')
